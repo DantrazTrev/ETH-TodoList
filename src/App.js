9c06fbc68f45a54035b6e5bf13db9c4c228826ac
Ayush Pratap Singh <ayushpratap16@gmail.com>
2021-12-10 23:38:31 +0530

    Base Todo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 App = {
+  loading: false,
+  contracts: {},
   load: async () => {
     await App.loadWeb3();
     await App.loadAccount();
+    await App.loadContract();
+    await App.render();
+    await App.renderTasks();
   },
   loadWeb3: async () => {
     if (typeof web3 !== 'undefined') {
@@ -42,7 +47,68 @@ App = {
   },
   loadAccount: async () => {
     App.account = web3.eth.accounts[0];
-    console.log(web3.eth.accounts[0]);
+    console.log(App.account);
+  },
+  loadContract: async () => {
+    const todolist = await jQuery.getJSON('TodoList.json');
+    App.contracts.ToDoList = TruffleContract(todolist);
+    App.contracts.ToDoList.setProvider(web3.currentProvider);
+    App.todoList = await App.contracts.ToDoList.deployed();
+  },
+  render: async () => {
+    if (App.loading) {
+      return;
+    }
+    App.setLoading(true);
+    $('#account').html(App.account);
+    App.setLoading(false);
+  },
+  renderTasks: async () => {
+    let taskcount = await App.todoList.taskCount();
+    const $taskTemplate = $('.taskTemplate');
+
+    taskcount = taskcount.toNumber();
+    for (let i = 1; i <= taskcount; i++) {
+      const task = await App.todoList.tasks(i);
+
+      console.log(task);
+      const taskId = task[0].toNumber();
+      const taskContent = task[1];
+      const taskCompleted = task[2];
+      const $newTaskTemplate = $taskTemplate.clone();
+      $newTaskTemplate.find('.content').html(taskContent);
+      $newTaskTemplate
+        .find('input')
+        .prop('name', taskId)
+        .prop('checked', taskCompleted);
+      // .on('click', App.toggleCompleted);
+      if (taskCompleted) {
+        $('#completedTaskList').append($newTaskTemplate);
+      } else {
+        $('#taskList').append($newTaskTemplate);
+      }
+      $newTaskTemplate.show();
+    }
+  },
+
+  createTask: async () => {
+    App.setLoading(true);
+    const content = $('#newTask').val();
+    await App.todoList.createTask(content, { from: App.account });
+    window.location.reload();
+  },
+
+  setLoading: (boolean) => {
+    App.loading = boolean;
+    const loader = $('#loader');
+    const content = $('#content');
+    if (boolean) {
+      loader.show();
+      content.hide();
+    } else {
+      loader.hide();
+      content.show();
+    }
   },
 };
 
